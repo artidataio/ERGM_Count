e99c40ab3f0196bcc3336247ecfe3d6dc54e2879
imaduddin haetami <khattami240193@gmail.com>
2017-03-11 16:02:10 +0800

    Something new to write

--- scripts/Table.R
+++ scripts/Table.R
@@ -12,7 +12,7 @@ table_LS <- list()
 
 for(names in names(coef_LS)){
   DT <- coef_LS[[names]]
-  rgroup <- as.roman(1:6)
+  rgroup <- (1:6)
   n.rgroup <- rep(nrow(DT)/6,6)
 
   z <- ztable(DT[,c(1,2,3,5)],digits = 4)
@@ -22,5 +22,24 @@ for(names in names(coef_LS)){
   table_LS[[stri_sub(names,1,-4)]] <- z
 }
 
+poisson_DT <- coef_LS$poisson_DT
+geometric_DT <- coef_LS$geometric_DT
+pois_geom_table <- ztable(cbind(poisson_DT[,c(1,2,3,5)],geometric_DT[,c(2,3,5)]))
+pois_geom_table <- addCellColor(pois_geom_table,
+                                rows = rep(poisson_DT[`p-value`<0.1,which=T]+1,3),
+                                cols = rep(c(3,4,5),each = length(poisson_DT[`p-value`<0.1,which=T])),
+                                color ="platinum")
+pois_geom_table <- addCellColor(pois_geom_table,
+                                rows = rep(geometric_DT[`p-value`<0.1,which=T]+1,3),
+                                cols = rep(c(6,7,8),each = length(geometric_DT[`p-value`<0.1,which=T])),
+                                color ="platinum")
+
+pois_geom_table <- addcgroup(pois_geom_table,
+                             cgroup = c("", "Poisson","Geometric"),
+                             n.cgroup = c(1,3,3))
+pois_geom_table <- vlines(pois_geom_table,
+                          add=c(3,6))
+
+attr(pois_geom_table$x,"footer")<-"Highlighted parameters are significant at 0.1 level"
 saveRDS(table_LS,paste(root,"/processed data/table_LS",sep=""))
 
